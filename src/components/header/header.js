1286fc149e4c2c40aa7792cd8bf331d817066667
Lamont Crook <lamont@adobe.com>
2024-02-11 14:34:44 -0700

    update nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,18 +15,51 @@ import ModelManager from '../../utils/modelmanager';
 import './header.css';
 
 const Header = ({ content, config }) => {
+  const language = localStorage.getItem('lang') || 'en';
+
   return (
     <React.Fragment>
       <header className='header' role='banner'>
         <div className="promo-banner-content">
-          <ul className="ds-promo-line-1">
-            <li>Member Exclusive! $17.95 3-Wick &amp; Mist</li>
-            <li>Sign in &amp; shop</li>
-          </ul>
-          <ul>
-            <li>Limited time only!</li>
-            <li>*Promo Details</li>
-          </ul>
+          {language === 'en' && (
+            <React.Fragment>
+              <ul className='ds-promo-line-1'>
+                <li>Member Exclusive! $17.95 3-Wick &amp; Mist</li>
+                <li>Sign in &amp; shop</li>
+              </ul>
+              <ul>
+                <li>Limited time only!</li>
+                <li>*Promo Details</li>
+              </ul>
+            </React.Fragment>
+          )}
+
+          {language === 'fr' && (
+            <React.Fragment>
+              <ul className='ds-promo-line-1'>
+                <li>Exclusivité réservée aux membres ! 17,95 $ 3 mèches et ampli ; Brume</li> {/* eslint-disable-line no-irregular-whitespace */}
+                <li>Connectez-vous etamp; boutique</li>
+              </ul>
+              <ul>
+                <li>Durée limitée !</li> {/* eslint-disable-line no-irregular-whitespace */}
+                <li>*Détails de la promotion</li>
+              </ul>
+            </React.Fragment>
+          )}
+
+          {language === 'es' && (
+            <React.Fragment>
+              <ul className='ds-promo-line-1'>
+                <li>¡Exclusivo para miembros! $17,95 3 mechas y mechas niebla</li>
+                <li>Iniciar sesión y acceder comprar</li>
+              </ul>
+              <ul>
+                <li>¡Solo por tiempo limitado!</li>
+                <li>*Detalles de la promoción</li>
+              </ul>
+            </React.Fragment>
+          )}
+
         </div>
         <div className='logo'>
           {config && config.configurationByPath && config.configurationByPath.item && (
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -5,18 +5,45 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   const [expanded, setExpanded] = useState(false);
+  const language = localStorage.getItem('lang') || 'en';
 
   let obj = {
-    pos1: { name: 'Top Offers', path: '/site/en/top-offers/top-offers' },
-    pos2: { name: 'Body Care', path: '#' },
-    pos3: { name: 'Candles', path: '#' },
-    pos4: { name: 'Wallflowers & Air Fresheners', path: '#' },
-    pos5: { name: 'Hand Soaps & Sanitizers', path: '#' },
-    pos6: { name: 'Men\'s Shop', path: '#' },
-    pos7: { name: 'Laundry Care', path: '#' },
-    pos8: { name: 'Top Fragrances', path: '#' },
-    pos9: { name: 'New & Now', path: '#' },
-    pos10: { name: 'Settings', path: '/settings' },
+    en: {
+      pos1: { name: 'Top Offers', path: '/site/en/top-offers/top-offers' },
+      pos2: { name: 'Body Care', path: '#' },
+      pos3: { name: 'Candles', path: '#' },
+      pos4: { name: 'Wallflowers & Air Fresheners', path: '#' },
+      pos5: { name: 'Hand Soaps & Sanitizers', path: '#' },
+      pos6: { name: 'Men\'s Shop', path: '#' },
+      pos7: { name: 'Laundry Care', path: '#' },
+      pos8: { name: 'Top Fragrances', path: '#' },
+      pos9: { name: 'New & Now', path: '#' },
+      pos10: { name: 'Settings', path: '/settings' },
+    },
+    fr: {
+      pos1: { name: 'Meilleures offres', path: '/site/en/top-offers/top-offers' },
+      pos2: { name: 'Soins du corps', path: '#' },
+      pos3: { name: 'Bougies', path: '#' },
+      pos4: { name: 'Giroflées et assainisseurs d\'air', path: '#' },
+      pos5: { name: 'Savons et désinfectants pour les mains', path: '#' },
+      pos6: { name: 'Boutique pour hommes', path: '#' },
+      pos7: { name: 'Entretien du linge', path: '#' },
+      pos8: { name: 'Top Fragrances', path: '#' },
+      pos9: { name: 'Nouveau et maintenant', path: '#' },
+      pos10: { name: 'Paramètres', path: '/settings' },
+    },
+    es: {
+      pos1: { name: 'Ofertas principales', path: '/sitio/es/ofertas-principales/ofertas-principales' },
+      pos2: { name: 'Cuidado del cuerpo', path: '#' },
+      pos3: { name: 'Velas', path: '#' },
+      pos4: { name: 'Alhelíes y ambientadores', path: '#' },
+      pos5: { name: 'Jabones y desinfectantes para manos', path: '#' },
+      pos6: { name: 'Tienda de hombres', path: '#' },
+      pos7: { name: 'Cuidado de la ropa', path: '#' },
+      pos8: { name: 'Fragancias principales', path: '#' },
+      pos9: { name: 'Nuevo y ahora', path: '#' },
+      pos10: { name: 'Configuración', path: '/configuración' },
+    }
   };
 
   return (
@@ -31,7 +58,7 @@ const Navigation = () => {
         </div>
         <div className='nav-sections'>
           <ul>
-            {Object.entries(obj).map(([key, {name, path}]) => (
+            {Object.entries(obj[language]).map(([key, { name, path }]) => (
               <li key={key}><Link to={path} className={'navItem'} name={name}>{name}</Link></li>
             ))}
           </ul>
